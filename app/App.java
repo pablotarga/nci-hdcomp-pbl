fff9811f2b757efc65c2c750d9be5bdf3632ddaf
Shane Gibney <shanegibney@gmail.com>
2019-11-23 16:46:25 +0000

    initial commit for line class

--- app/App.java
+++ app/App.java
@@ -5,14 +5,25 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.Scanner;
+
 public class App
 {
-    public static void main(String[] args){
+public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
         History h = new History();
-        
-        for(int i = 0; i < 25; i++){
-            
-            h.store( new Game() );
+
+        for(int i = 0; i < 25; i++) {
+
+                h.store( new Game() );
         }
-    }
+
+        Line l = new Line(6, 1, 40);
+        do {
+                System.out.println("Choose a number from 1 - 40.");
+                l.add(input.nextInt());
+        } while(!l.isFull());
+        // l.add(3);
+}
 }
