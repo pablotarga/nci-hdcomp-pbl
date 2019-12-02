7dc7ba78a32f6bcca7078c18cc9163e7fe324858
Pablo Targa <pablo.targa@gmail.com>
2019-12-02 20:53:20 +0000

    Add method play and helpers methods on main class

--- app/App.java
+++ app/App.java
@@ -12,40 +12,65 @@ public class App
 {
 public static void main(String[] args){
 
-        Deck d = new Deck(6,1,15);//6 number of slots
-        Deck b = new Deck(6,1,15);
-        Printer.printLine(b);
-        Printer.printLine(d);
-        d.check(b);
-        Printer.printLine(d);
-        System.out.println(d.getHits());
+        // Deck d = new Deck(6,1,15);//6 number of slots
+        // Deck b = new Deck(6,1,15);
+        // Printer.printLine(b);
+        // Printer.printLine(d);
+        // d.check(b);
+        // Printer.printLine(d);
+        // System.out.println(d.getHits());
+        History history = play();
+        Printer.printHistory(history);
 
-        // Scanner input = new Scanner(System.in);
-        // // History h = new History();
-        // //
-        // // for(int i = 0; i < 25; i++) {
-        // //
-        // //         h.store( new Game() );
-        // // }
-        // Random r = new Random();
-        // Line l = new Line(6, 1, 40);
-        // do {
-        //         System.out.println("Choose a number from 1 - 40.");
-        //         l.add(r.nextInt(39)+1);
-        //         // l.add(input.nextInt());
-        //         l.print();
-        // } while(!l.isFull());
-        //
-        // Game g = new Game();
-        //
-        // g.addLine(l);
-        // Printer.printGame(g);
-        //
-        // g.addLine(l);
-        // Printer.printGame(g);
-        //
-        // g.addLine(l);
-        // Printer.printGame(g);
-        // // l.add(3);
+
+}
+
+public static boolean yn(String question, Scanner s){
+        System.out.print(Printer.warning(question+" [y/n]"));
+        char ans = s.next().trim().toLowerCase().charAt(0);
+        return ans == 'y';
+}
+
+public static void askDigit(Line l, Scanner s){
+        System.out.print("Please inform a digit from 1 to 40: ");
+        int n = s.nextInt();
+        if(l.add(n)){
+                System.out.println(Printer.success(n + " was added to the line!"));
+        } else {
+                System.out.println(Printer.error("Informed digit is not valid please check if between 1 and 40 and not included on the line"));
+                System.out.print("Line: ");
+                Printer.printLine(l);
+        }
+}
+
+public static History play(){
+        Scanner s = new Scanner(System.in);
+
+        History history = new History();
+        Printer.greetings();
+        Printer.rules();
+        do{
+                Game game = new Game();
+                while(game.acceptsNewLine()){
+                        if(!yn("Want to play a line?", s)){
+                                game.noMoreBets();
+                                break;
+                        }
+
+                        Line l = new Line(6, 1, 40);
+                        do{
+                                askDigit(l, s);
+                        } while(!l.isFull());
+
+                        Printer.printLine(l);
+                        if(yn("Do you confirm the above line?", s)){
+                                game.addLine(l);
+                        }
+                }
+
+                history.store(game);
+        } while(yn("Want to play another game?", s));
+
+        return history;
 }
 }
