65d528597a97e0a6d0e96d50fe366d03cb5287b3
Pablo Targa <pablo.targa@gmail.com>
2019-12-07 11:40:41 +0000

    Add bright yellow color, rainbow and gold format

--- app/Colorize.java
+++ app/Colorize.java
@@ -9,24 +9,49 @@ public class Colorize{
   public static final String ANSI_CYAN = "\u001B[36m";
   public static final String ANSI_WHITE = "\u001B[37m";
 
+  public static final String ANSI_BRIGHT_YELLOW = "\u001B[93m";
+
+
+  public static String f(String color, char c){
+        return f(color, Character.toString(c));
+  }
 
   public static String f(String color, String text){
-    return String.format("%s%s%s", color, text, ANSI_RESET);
+        return String.format("%s%s%s", color, text, ANSI_RESET);
   }
 
   public static String error(String string){
-          return f(ANSI_RED, string);
+        return f(ANSI_RED, string);
   }
 
   public static String info(String string){
-          return f(ANSI_CYAN, string);
+        return f(ANSI_CYAN, string);
   }
 
   public static String success(String string){
-          return f(ANSI_GREEN, string);
+        return f(ANSI_GREEN, string);
   }
 
   public static String warning(String string){
-          return f(ANSI_YELLOW, string);
+        return f(ANSI_YELLOW, string);
   }
+
+  public static String gold(String string){
+        return f(ANSI_BRIGHT_YELLOW, string);
+  }
+
+  public static String rainbow(String string){
+          String[] colors = {ANSI_RED, ANSI_GREEN, ANSI_YELLOW, ANSI_BLUE, ANSI_PURPLE, ANSI_CYAN};
+          int cLen = colors.length;
+
+          StringBuffer sb = new StringBuffer();
+          for(int i = 0; i < string.length(); i++){
+                String colorizedLetter = f(colors[i%cLen], string.charAt(i));
+                sb.append(colorizedLetter);
+          }
+
+          return sb.toString();
+  }
+
+
 }
