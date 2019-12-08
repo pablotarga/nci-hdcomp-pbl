6147e6ae37ebba0ff3472711ac872aab18ffecc1
Pablo Targa <pablo.targa@gmail.com>
2019-12-08 21:44:55 +0000

    Add comments on the Colorize class

--- app/Colorize.java
+++ app/Colorize.java
@@ -1,6 +1,8 @@
 /**
  * Colorize Class
  *
+ * This is a helper class that colorize the output to the console.
+ * It is heavily used on the Printer class
  * @author Pablo Targa
  * @version 2019-12-07
  */
@@ -17,35 +19,42 @@ public static final String ANSI_WHITE = "\u001B[37m";
 
 public static final String ANSI_BRIGHT_YELLOW = "\u001B[93m";
 
-
+// format function that takes a color and char, and format the char as string with the color
 public static String f(String color, char c){
         return f(color, Character.toString(c));
 }
 
+// format function that takes a color and string, and format the string with the color
 public static String f(String color, String text){
         return String.format("%s%s%s", color, text, ANSI_RESET);
 }
 
+// format the string with red color to symbolize error
 public static String error(String string){
         return f(ANSI_RED, string);
 }
 
+// format the string with cyan color to symbolize information
 public static String info(String string){
         return f(ANSI_CYAN, string);
 }
 
+// format the string with green color to symbolize success
 public static String success(String string){
         return f(ANSI_GREEN, string);
 }
 
+// format the string with yellow (ambar) color to symbolize warning
 public static String warning(String string){
         return f(ANSI_YELLOW, string);
 }
 
+// format the string with light yellow color to symbolize gold
 public static String gold(String string){
         return f(ANSI_BRIGHT_YELLOW, string);
 }
 
+// uses a string buffer to format each letter of the string with a different color
 public static String rainbow(String string){
         String[] colors = {ANSI_RED, ANSI_YELLOW, ANSI_GREEN, ANSI_CYAN, ANSI_BLUE, ANSI_PURPLE};
         int cLen = colors.length;
