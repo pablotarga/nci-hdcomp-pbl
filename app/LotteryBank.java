50e2d1c618b04e3a2f7653284f7d11b1b8ffa92b
Shane Gibney <shanegibney@gmail.com>
2019-12-08 18:06:50 +0000

    trivial updated

--- app/LotteryBank.java
+++ app/LotteryBank.java
@@ -1,84 +1,90 @@
+/**
+ * LotteryBank Class
+ *
+ * @author Pablo Targa
+ * @version 2019-12-07
+ */
 public class LotteryBank {
-  public static final double PRICE_PER_LINE = 2.5;
-  double balance;
-  double initialBalance;
-  double securityMargin;
-  int linesCashedIn;
-  int prizesPaid;
-  double paidSum;
+public static final double PRICE_PER_LINE = 2.5;
+double balance;
+double initialBalance;
+double securityMargin;
+int linesCashedIn;
+int prizesPaid;
+double paidSum;
 
-  LotteryBank(double initialBalance){
-    balance = this.initialBalance = initialBalance;
+LotteryBank(double initialBalance){
+        balance = this.initialBalance = initialBalance;
 
-    securityMargin = balance * 0.05;
-    if(securityMargin < 10000){
-      securityMargin = 10000;
-    }
+        securityMargin = balance * 0.05;
+        if(securityMargin < 10000) {
+                securityMargin = 10000;
+        }
 
-    linesCashedIn = 0;
-    paidSum = 0.0;
-  }
+        linesCashedIn = 0;
+        paidSum = 0.0;
+}
 
-  public void cashIn(){
-    balance += PRICE_PER_LINE;
-    linesCashedIn++;
-  }
+public void cashIn(){
+        balance += PRICE_PER_LINE;
+        linesCashedIn++;
+}
 
-  public double pay(Line l){
-    double value = getPrizeAmount(l);
-    balance -= value;
-    prizesPaid++;
-    paidSum += value;
+public double pay(Line l){
+        double value = getPrizeAmount(l);
+        balance -= value;
+        prizesPaid++;
+        paidSum += value;
 
-    return value;
-  }
+        return value;
+}
 
-  public double getCachedInSum(){
-    return linesCashedIn * PRICE_PER_LINE;
-  }
-  public double getPaidSum(){
-    return paidSum;
-  }
+public double getCachedInSum(){
+        return linesCashedIn * PRICE_PER_LINE;
+}
+public double getPaidSum(){
+        return paidSum;
+}
 
-  public int getPrizesPaid(){
-    return prizesPaid;
-  }
+public int getPrizesPaid(){
+        return prizesPaid;
+}
 
-  public double getBalance(){
-    return balance;
-  }
+public double getBalance(){
+        return balance;
+}
 
-  public double getInitialBalance(){
-    return initialBalance;
-  }
+public double getInitialBalance(){
+        return initialBalance;
+}
 
-  public int getLinesCachedIn(){
-    return linesCashedIn;
-  }
+public int getLinesCachedIn(){
+        return linesCashedIn;
+}
 
-  public boolean hasBalance(){
-    return balance > securityMargin;
-  }
+public boolean hasBalance(){
+        return balance > securityMargin;
+}
 
-  private double getPrizeAmount(Line l){
-    int hits = l.getAmountOfHits();
-    double value = 0;
+private double getPrizeAmount(Line l){
+        int hits = l.getAmountOfHits();
+        double value = 0;
 
-    switch(hits) {
-    case 3:
-      value = 125;
-      break;
-    case 4:
-      value = 300;
-      break;
-    case 5:
-      value = 1500;
-      break;
-    case 6:
-      value = balance;
-      break;
-    }
+        switch(hits) {
+        case 3:
+                value = 125;
+                break;
+        case 4:
+                value = 300;
+                break;
+        case 5:
+                value = 1500;
+                break;
+        case 6:
+                value = balance;
+                break;
+        }
 
-    return value;
-  }
+        return value;
+}
 }
